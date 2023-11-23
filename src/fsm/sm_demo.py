1b52feba15f99bc1a3809b763ea08808a6b2e4c6
zingff <daifeifan@gmail.com>
2023-11-23 11:02:59 +0800

    update fsm

--- src/fsm/sm_demo.py
+++ src/fsm/sm_demo.py
@@ -15,34 +15,53 @@ import smach_ros
 
 import std_srvs.srv
 from tf2_msgs import msg
-# from tree_of_thougnt.arm import output
-import turtlesim.srv
 import anygrasp_generation.srv
 import kortex_motion_planning.srv
 from geometry_msgs.msg import Pose, Transform
 from trajectory_msgs.msg import JointTrajectory
+import tf2_ros
 
 outcomes_sm = ['motion_generator'
     , 'grasp_generator'
     , 'motion_executor'
     , 'initial_motion_generator'
-    , 'succeeded', 'aborted', 'preempted'
+    , 'succeeded'
+    , 'aborted'
+    , 'preempted'
                ]
 
-input_keys_sm = ['ud_motion_plan'
-    , 'ud_success']
+# Input and output keys for the states
+input_keys_sm = ['motion_plan'
+    # , 'ud_success'
+    # , 'ud_update_anygrasp'
+    , 'update_anygrasp'
+    , 'success'
+    , 'anygrasp_transforms'
+    # , 'ud_anygrasp_transforms'
+    , 'ud_success'
+    , 'ud_anygrasp_transforms'
+    , 'ud_anygrasp_transform'
+    , 'anygrasp_transform'
+                 ]
+
+# output_keys=['ud_success', 'ud_anygrasp_transforms']
 
 transition_sm = {
-    'motion_generator': 'motion_generator'
+      'motion_generator': 'motion_generator'
     , 'grasp_generator': 'grasp_generator'
     , 'motion_executor': 'motion_executor'
     , 'initial_motion_generator': 'initial_motion_generator'
 }
 
 # from internal state machine to global variables
+# keys used within the specific state: variable in sm's userdata (i.e., sm.userdata)
+# io keys: userdata.data
 remapping_sm = {
-    'ud_motion_plan': 'motion_plan'
-    , 'ud_success': 'success'
+      'motion_plan': 'ud_motion_plan'
+    , 'update_anygrasp': 'ud_update_anygrasp'
+    , 'anygrasp_transforms': 'ud_anygrasp_transforms'
+    , 'success': 'ud_success'
+    , 'anygrasp_transform': 'ud_anygrasp_transform'
 }
 
 
@@ -58,7 +77,7 @@ def motion_generator_callback(userdata, response):
 
         return 'motion_executor'
     else:
-        rospy.warn("motion_generator: failed")
+        rospy.logwarn("motion_generator: failed")
         return 'motion_generator'
 
 
@@ -71,11 +90,14 @@ def motion_executor_callback(userdata, response):
 
 
 def grasp_generator_callback(userdata, response):
-    # if response.success:
-    #     return 'wait'
     if response.success:
         rospy.loginfo("grasp_generator: success")
+        userdata.ud_success = True
         return 'initial_motion_generator'
+    else:
+        rospy.logwarn("grasp_generator: failed")
+        userdata.ud_success = False
+        return 'failed'
 
 
 class wait(smach.State):
@@ -87,11 +109,12 @@ class wait(smach.State):
 
 
 class grasp_generator(smach_ros.ServiceState):
-    def __init__(self, update_anygrasp, input_keys_sm, outcomes_sm):
+    def __init__(self, request_key_, input_keys_sm, outcomes_sm):
         super(grasp_generator, self).__init__(
             service_name='/grasp_generator',
             service_spec=anygrasp_generation.srv.AnyGraspGeneration,
-            request=anygrasp_generation.srv.AnyGraspGenerationRequest(update_anygrasp),
+            # request=anygrasp_generation.srv.AnyGraspGenerationRequest(update_anygrasp),
+            request_key=request_key_,
             response_slots=['anygrasp_transforms', 'success'],
             outcomes=outcomes_sm,
             input_keys=input_keys_sm,
@@ -99,6 +122,29 @@ class grasp_generator(smach_ros.ServiceState):
             response_cb=grasp_generator_callback
             # response_cb=motion_generator_callback
         )
+        self.anygrasp_tf_broadcaster = tf2_ros.TransformBroadcaster()
+        self.grasp_namespace = 'anygrasp'
+
+    def execute(self, userdata):
+        # Call parent execute (makes the service call)
+        outcome = super(grasp_generator, self).execute(userdata)
+
+        # Publish TF transforms if the service call was successful
+        if userdata.success:
+            userdata.ud_anygrasp_transform = userdata.anygrasp_transforms[0]
+            print('The best grasp from AnyGrasp: ')
+            print(userdata.anygrasp_transform)
+            # Publish each transform once
+            for i, transform in enumerate(userdata.anygrasp_transforms):
+                anygrasp_tf_msg = tf2_ros.TransformStamped()
+                anygrasp_tf_msg.header.stamp = rospy.Time.now()
+                anygrasp_tf_msg.header.frame_id = 'base_link'
+                anygrasp_tf_msg.child_frame_id = f'{self.grasp_namespace}/grasp_{i}'
+                anygrasp_tf_msg.transform.translation = transform.translation
+                anygrasp_tf_msg.transform.rotation = transform.rotation
+                self.anygrasp_tf_broadcaster.sendTransform(anygrasp_tf_msg)
+
+        return outcome
 
 
 class motion_generator(smach_ros.ServiceState):
@@ -138,6 +184,7 @@ def main():
     # sm = smach.StateMachine(outcomes=[])
     sm = CustomStateMachine(outcomes_sm, input_keys=input_keys_sm, output_keys=input_keys_sm)
 
+    # Initialize necessary data in feeding task
     # food_transfer
     feeding_pose = Pose()
     feeding_pose.position.x = -0.15135
@@ -157,20 +204,31 @@ def main():
     target_pose_2.orientation.z = -0.604657
     target_pose_2.orientation.w = 0.4928685
 
-    update_anygrasp = True
+    # update_anygrasp = True
 
     sm.userdata.ud_motion_plan = JointTrajectory()
+    sm.userdata.ud_update_anygrasp = True
     sm.userdata.ud_success = False
     sm.userdata.ud_message = ''
     sm.userdata.ud_anygrasp_transforms = []
+    sm.userdata.ud_anygrasp_transform = Transform()
 
     # Remap variables
     sm.userdata.ud_feeding_pose = feeding_pose
     sm.userdata.ud_target_pose_2 = target_pose_2
-    sm.userdata.ud_update_anygrasp = update_anygrasp
+    # sm.userdata.ud_update_anygrasp = update_anygrasp
 
     # Open the container
     with sm:
+        # with sm:
+        #     # Other states...
+        #     smach.StateMachine.add('grasp_generator',
+        #                            grasp_generator('ud_update_anygrasp', ['ud_update_anygrasp'],
+        #                                            ['ud_success', 'ud_anygrasp_transforms'], outcomes_sm),
+        #                            transitions=transition_sm,
+        #                            remapping={'update_anygrasp': 'ud_update_anygrasp',
+        #                                       'anygrasp_transforms': 'ud_anygrasp_transforms',
+        #                                       'success': 'ud_success'})
         smach.StateMachine.add('motion_generator'
                                , motion_generator(sm.userdata.ud_feeding_pose
                                                   , input_keys_sm
@@ -184,7 +242,7 @@ def main():
                                , transitions=transition_sm
                                , remapping=remapping_sm)
         smach.StateMachine.add('grasp_generator'
-                               , grasp_generator(sm.userdata.ud_update_anygrasp
+                               , grasp_generator('update_anygrasp'
                                                  , input_keys_sm
                                                  , outcomes_sm)
                                , transitions=transition_sm
